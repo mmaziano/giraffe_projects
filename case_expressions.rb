458f7a2e483a03f610ebd853244f8f6231c969df
Mason Maziano <mason.k.maziano@gmail.com>
2022-07-23 15:25:55 +0100

    Example of changes

--- case_expressions.rb
+++ case_expressions.rb
@@ -1,10 +1,9 @@
 def get_day_name(day)
-    day_name = ""
+	day_name = ""
 
-if day == "mon"
-    day_name = "monday"
-elsif day == "tue"
-    day_name = "tuesday"
-
-    return day_name
+	if day == "mon"
+		day_name = "monday"
+	elsif day == "tue"
+		day_name = "tuesday"
+	return day_name
 end
