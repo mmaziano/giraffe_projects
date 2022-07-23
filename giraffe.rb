0bc1ad170953e4248f76159b18c5fa64ab3fb309
Mason Maziano <mason.k.maziano@gmail.com>
2022-07-23 14:58:46 +0100

    Girafe Academy Projects

---
+++ giraffe.rb
@@ -0,0 +1,22 @@
+character_name = "Mason"
+character_age = "12"
+puts ("Hello my name is " + character_name)
+puts "I am " + character_age 
+character_name = "Mike"
+character_age = "23"
+puts "I am called " + character_name
+puts ("And yes, i am " + character_age + " years old")
+character_name = "Kayla"
+character_age = "8"
+puts "Hello,i am " + character_name
+puts ("I am " + character_age + " years old")
+character_name = "Crista Ronaldo"
+character_age = "18.5"
+puts "Hello i am " + character_name
+puts ("And i am " + character_age + " years old, siiuuuu!!")
+character_name = "Spiderman"
+character_age = "17"
+puts "I am " + character_name
+puts "I am " + character_age
+
+
---
+++ mad_libs.rb
@@ -0,0 +1,21 @@
+puts "Enter a color:"
+print "> "
+color = gets.chomp
+puts "Enter a plural noun"
+print "> "
+plural_noun = gets.chomp
+puts "Enter a celebrity"
+print "> "
+celebrity = gets.chomp
+puts "Enter a formula of the circle or the volume of the cilindre"
+print "> "
+math = gets.chomp
+
+
+
+
+
+puts ("Roses are #{color}")
+puts ("#{plural_noun} are blue")
+puts ("I love #{celebrity}")
+puts ("So the formula or the volume are #{math}")
