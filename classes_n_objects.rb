f0e08becc638e6ac08c50d20326bd39329215e21
Mason Maziano <mason.k.maziano@gmail.com>
2022-08-05 14:39:35 +0100

    New Project

--- classes_n_objects.rb
+++ classes_n_objects.rb
@@ -10,28 +10,39 @@
 #book1 = Book.new("Harry Potter", "JK Rowling", 400)
 
 class Person
-    attr_accessor :birthyear, :first_name, :last_name, :height
+    attr_accessor :birthyear, :first_name, :last_name, :height, :gender
 
-    def full_name()
-      "#{first_name} #{last_name}"
+    def initialize(name, gender)
+      @first_name = name
+      @last_name = "Bean"
+      @gender = gender
+    end
+
+    def full_name
+      family = ['jonh', 'michel', 'jason', 'jessica', 'patrick']
+      if family.include?(@first_name.downcase)
+        # true
+        "#{first_name} #{last_name}"
+      else
+        # false
+        "#{@first_name} is not a Bean"
+      end
     end
 
 		def age
 			2022 - birthyear
-		end
-end
+    end
+  end
 
-first_person = Person.new
-first_person.first_name = "Mason"
-first_person.last_name = "Maziano"
+first_person = Person.new("Patrick", "Male")
 first_person.birthyear = 2010
+first_person.height = 5
 
-second_person = Person.new
-second_person.first_name = "Sergio"
-second_person.last_name = "Maziano"
-second_person.birthyear = 1984
+second_person = Person.new("Jessica", "Female")
+second_person.birthyear = 2014
 
-p first_person.full_name
-p first_person.age
-p second_person.full_name.upcase
-p second_person.age.to_s
+puts first_person.full_name
+puts first_person.age
+
+puts second_person.full_name.upcase
+puts second_person.age.to_s
